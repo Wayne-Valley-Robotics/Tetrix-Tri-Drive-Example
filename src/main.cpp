511e01eb89b98340da31a4d14b02f2863529e7a2
okm <64970074+plexede@users.noreply.github.com>
2026-01-05 16:29:15 -0500

    brake logic and notation

--- src/main.cpp
+++ src/main.cpp
@@ -5,31 +5,34 @@ PRIZM prizm;
 EXPANSION exc;
 PS4 ps4;
 
-const int DEADZONE = 10;        // joystick deadzone range
-const int MOTOR_MAX_SPEED = 50; // motor power level (0-100)
+// Constants
+const int DEADZONE = 10;            // joystick deadzone range
+const int MOTOR_MAX_SPEED = 50;     // motor power level (0-100)
+const bool BRAKE_ON_RELEASE = true; // i got bored
 
 const int MOTOR_FRONT_EXPANSION = 1; // front motor expansion address
 const int MOTOR_FRONT_PORT = 1;      // front motor port
 
 const int MOTOR_LEFT_EXPANSION = 1; // left motor expansion address
-const int MOTOR_LEFT_PORT = 2;       // left motor port
+const int MOTOR_LEFT_PORT = 2;      // left motor port
 
 const int MOTOR_RIGHT_EXPANSION = 1; // right motor expansion address
 const int MOTOR_RIGHT_PORT = 2;      // right motor port
-// Initialize motor powers
+
+// Initialize motor powers so as to avoid reinitialization each loop
 int powerFront = 0;
 int powerLeft = 0;
 int powerRight = 0;
 
 void setup()
 {
-  prizm.PrizmBegin();
+  prizm.PrizmBegin(); // tetrix is garbage
 }
 
 void loop()
 {
   int motorSpeed = 0;
-  // store joystick values to avoid streneuously calling this stupid library
+  // store joystick values to avoid streneuously calling this stupid library (these three alone should take more than 100ms alone)
   int joyLY = ps4.Stick(LY);
   int joyLX = ps4.Stick(LX);
   int joyRX = ps4.Stick(RX);
@@ -45,52 +48,64 @@ void loop()
   int btnRotL = (joyRX * (joyRX < -DEADZONE)); // rotate left
   int btnRotR = (joyRX * (joyRX > DEADZONE));  // rotate right
 
-  // Forward/back
-  if (btnUp > 0)
-  {
-    motorSpeed = map(btnUp, 0, 255, 0, MOTOR_MAX_SPEED);
-    powerFront += motorSpeed;
-    powerLeft += motorSpeed;
-    powerRight += motorSpeed;
-  }
-  if (abs(btnDown) > 0)
-  {
-    motorSpeed = map(abs(btnDown), 0, 255, 0, MOTOR_MAX_SPEED);
-    powerFront -= motorSpeed;
-    powerLeft -= motorSpeed;
-    powerRight -= motorSpeed;
-  }
+  // Input logic
 
-  // Strafe left/right (triangle drive math)
-  if (abs(btnLeft) > 0)
+  // brake on release logic
+  if ((abs(btnUp) + abs(btnDown) + abs(btnLeft) + abs(btnRight) + abs(btnRotL) + abs(btnRotR)) > 0) // if input from any directions, else skip eval logic and brake
   {
-    motorSpeed = map(abs(btnLeft), 0, 255, 0, MOTOR_MAX_SPEED);
-    powerLeft += motorSpeed;
-    powerRight -= motorSpeed;
-  }
-  if (abs(btnRight) > 0)
-  {
-    motorSpeed = map(abs(btnRight), 0, 255, 0, MOTOR_MAX_SPEED);
-    powerLeft -= motorSpeed;
-    powerRight += motorSpeed;
-  }
+    // Forward/back input processing
+    if (btnUp > 0)
+    {
+      motorSpeed = map(btnUp, 0, 255, 0, MOTOR_MAX_SPEED);
+      powerFront += motorSpeed;
+      powerLeft += motorSpeed;
+      powerRight += motorSpeed;
+    }
+    if (abs(btnDown) > 0)
+    {
+      motorSpeed = map(abs(btnDown), 0, 255, 0, MOTOR_MAX_SPEED);
+      powerFront -= motorSpeed;
+      powerLeft -= motorSpeed;
+      powerRight -= motorSpeed;
+    }
 
-  // Rotate
-  if (abs(btnRotL) > 0)
-  {
-    motorSpeed = map(abs(btnRotL), 0, 255, 0, MOTOR_MAX_SPEED);
-    powerFront -= motorSpeed;
-    powerLeft += motorSpeed;
-    powerRight -= motorSpeed;
-  }
-  if (abs(btnRotR) > 0)
-  {
-    motorSpeed = map(abs(btnRotR), 0, 255, 0, MOTOR_MAX_SPEED);
-    powerFront += motorSpeed;
-    powerLeft -= motorSpeed;
-    powerRight += motorSpeed;
-  }
+    // Strafe left/right input processing (triangle drive logic)
+    if (abs(btnLeft) > 0)
+    {
+      motorSpeed = map(abs(btnLeft), 0, 255, 0, MOTOR_MAX_SPEED);
+      powerLeft += motorSpeed;
+      powerRight -= motorSpeed;
+    }
+    if (abs(btnRight) > 0)
+    {
+      motorSpeed = map(abs(btnRight), 0, 255, 0, MOTOR_MAX_SPEED);
+      powerLeft -= motorSpeed;
+      powerRight += motorSpeed;
+    }
 
+    // Rotation input processing
+    if (abs(btnRotL) > 0)
+    {
+      motorSpeed = map(abs(btnRotL), 0, 255, 0, MOTOR_MAX_SPEED);
+      powerFront -= motorSpeed;
+      powerLeft += motorSpeed;
+      powerRight -= motorSpeed;
+    }
+    if (abs(btnRotR) > 0)
+    {
+      motorSpeed = map(abs(btnRotR), 0, 255, 0, MOTOR_MAX_SPEED);
+      powerFront += motorSpeed;
+      powerLeft -= motorSpeed;
+      powerRight += motorSpeed;
+    }
+  }
+  else
+  { // continuation of brake logic
+    // brake
+    powerFront = 125;
+    powerLeft = 125;
+    powerRight = 125;
+  }
   // Set motor powers
   exc.setMotorPower(MOTOR_FRONT_EXPANSION, MOTOR_FRONT_PORT, powerFront);
   exc.setMotorPower(MOTOR_LEFT_EXPANSION, MOTOR_LEFT_PORT, powerLeft);
